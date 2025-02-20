1ddf1f54d66a47354d694a6cfae8f3abe31c4196
lina <lina.benaissa@esprit.tn>
2025-02-20 13:03:25 +0100

    TP2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResidencesComponent } from './residences/residences.component';
+import { ResidenceDetailsComponent } from './residences/residence-details/residence-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'residences', component: ResidencesComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'residences/:id', component: ResidenceDetailsComponent }, // Route dynamique pour les détails
+  { path: '**', component:NotFoundComponent } // Redirige vers la liste des résidences si la route n'existe pas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
