a43d7e5a4c1d6c15d6b76053870c849ca196a375
lina <lina.benaissa@esprit.tn>
2025-02-20 12:10:33 +0100

    routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { ResidencesComponent } from './residences/residences.component';
+import { ResidenceDetailsComponent } from './residences/residence-details/residence-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AddResidenceComponent } from './residences/add-residence/add-residence.component';
+import { ApartmentsComponent } from './apartments/apartments/apartments.component';
+import { ApartmentsByResidenceComponent } from './apartments/apartments-by-residence/apartments-by-residence.component';
+import { AddApartmentComponent } from './apartments/add-apartment/add-apartment.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +21,13 @@ import { ResidencesComponent } from './residences/residences.component';
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-    ResidencesComponent
+    ResidencesComponent,
+    ResidenceDetailsComponent,
+    NotFoundComponent,
+    AddResidenceComponent,
+    ApartmentsComponent,
+    ApartmentsByResidenceComponent,
+    AddApartmentComponent
   ],
   imports: [
     BrowserModule,
